eb8797fb2d00180d44b293960fdd113e7271718a
Abraham Schilling <AbrahamSchilling@gmail.com>
2020-06-07 17:45:51 +0200

    Add more words

--- src/words.ts
+++ src/words.ts
@@ -1,4 +1,21 @@
-const awesomeWords = ["awesome", "good", "great", "amazing", "wow", "perfect"]
+const awesomeWords = [
+  "amazing",
+  "awesome",
+  "fascinating",
+  "good",
+  "great",
+  "incredible",
+  "marvelous",
+  "perfect",
+  "prodigious",
+  "shocking",
+  "stunning",
+  "surprising",
+  "terrific",
+  "unbelievable",
+  "wonderful",
+  "wow",
+]
 
 export function randomWord() {
   return awesomeWords[Math.floor(Math.random() * awesomeWords.length)]
