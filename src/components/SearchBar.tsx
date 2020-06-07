94e4e60857fff3127ba8e1c3db9607e8ba4f792c
Abraham Schilling <AbrahamSchilling@gmail.com>
2020-06-07 17:40:43 +0200

    Add clear button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,25 +9,33 @@ export type SearchBarProps = {
 
 export const SearchBar: FC<SearchBarProps> = ({ input, onInputChange }) => {
   return (
-    <input
-      className={styles.input}
-      type="search"
-      value={input}
-      onChange={(e) => onInputChange(e.target.value)}
-      spellCheck={false}
-      autoCapitalize="false"
-      autoComplete="false"
-      autoCorrect="false"
-      autoFocus
-      onBlur={(e) => {
-        const scrollTop = document.documentElement.scrollTop
-        e.target.focus()
-        document.documentElement.scrollTop = scrollTop
-      }}
-      onFocus={(e) => {
-        const length = e.target.value.length
-        e.target.setSelectionRange(length, length)
-      }}
-    />
+    <div className={styles.wrapper}>
+      <input
+        className={styles.input}
+        type="search"
+        value={input}
+        onChange={(e) => onInputChange(e.target.value)}
+        spellCheck={false}
+        autoCapitalize="false"
+        autoComplete="false"
+        autoCorrect="false"
+        autoFocus
+        onBlur={(e) => {
+          const scrollTop = document.documentElement.scrollTop
+          e.target.focus()
+          document.documentElement.scrollTop = scrollTop
+        }}
+        onFocus={(e) => {
+          const length = e.target.value.length
+          e.target.setSelectionRange(length, length)
+        }}
+      />
+
+      {input && (
+        <button
+          className={styles.clear}
+          onClick={(e) => onInputChange("")}></button>
+      )}
+    </div>
   )
 }
